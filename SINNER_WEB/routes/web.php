094664e1202a06f80a91c346168d3806cb2aed50
imffelipe <rs.mafla44@gmail.com>
2026-03-10 11:56:20 -0500

    Elaboración Rutas para las Vstas

--- SINNER_WEB/routes/web.php
+++ SINNER_WEB/routes/web.php
@@ -2,6 +2,32 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Página de Inicio
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+})->name('index');
+
+// Catálogo General
+Route::get('/catalogo', function () {
+    return view('catalogo');
+})->name('catalogo');
+
+// Streetwear
+Route::get('/catalogo/streetwear', function () {
+    return view('streetwear');
+})->name('streetwear');
+
+// Old Money
+Route::get('/catalogo/old-money', function () {
+    return view('oldmoney');
+})->name('oldmoney');
+
+// Nosotros
+Route::get('/nosotros', function () {
+    return view('nosotros');
+})->name('nosotros');
+
+// Soporte
+Route::get('/soporte', function () {
+    return view('soporte');
+})->name('soporte');
